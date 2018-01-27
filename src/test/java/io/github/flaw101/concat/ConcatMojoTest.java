b50ec447c0a3e8f138bd2ea28e1abfa8bf1f4d7e
Darren Forsythe <forsythed90@gmail.com>
2018-01-27 16:22:43 +0000

    correct formatting

--- src/test/java/io/github/flaw101/concat/ConcatMojoTest.java
+++ src/test/java/io/github/flaw101/concat/ConcatMojoTest.java
@@ -35,117 +35,135 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class ConcatMojoTest extends AbstractMojoTestCase {
-    private static String NEWLINE = System.getProperty("line.separator");
+	private static String NEWLINE = System.getProperty("line.separator");
 
-    /** {@inheritDoc} */
-    @Override
-    @Before
-    protected void setUp() throws Exception {
-        super.setUp();
-        FileUtils.deleteQuietly(new File("target/concatfile.output"));
-    }
+	/** {@inheritDoc} */
+	@Override
+	@Before
+	protected void setUp() throws Exception {
+		super.setUp();
+		FileUtils.deleteQuietly(new File("target/concatfile.output"));
+	}
 
-    /** {@inheritDoc} */
-    @Override
-    @After
-    protected void tearDown() throws Exception {
-        super.tearDown();
-    }
+	/** {@inheritDoc} */
+	@Override
+	@After
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
 
-    @Test
-    public void testConcat() throws Exception {
-        execute("src/test/resources/test-pom.xml", "concat", "target/concatfile.output", "file1file2file3");
-    }
+	@Test
+	public void testConcat() throws Exception {
+		execute("src/test/resources/test-pom.xml", "concat", "target/concatfile.output",
+				"file1file2file3");
+	}
 
-    @Test
-    public void testConcat_Directory() throws Exception {
-        execute("src/test/resources/test-pom-directory.xml", "concat", "target/concatfile.output", "file1file2file3");
-    }
+	@Test
+	public void testConcat_Directory() throws Exception {
+		execute("src/test/resources/test-pom-directory.xml", "concat",
+				"target/concatfile.output", "file1file2file3");
+	}
 
-    @Test
-    public void testConcatShouldNotDeleteTarget() throws Exception {
-        FileUtils.copyFile(new File("src/test/resources/testfiles/file_1.input"), new File("target/concatfile.output"));
-        execute("src/test/resources/test-pom.xml", "concat", "target/concatfile.output", "file1file1file2file3");
-    }
+	@Test
+	public void testConcatShouldNotDeleteTarget() throws Exception {
+		FileUtils.copyFile(new File("src/test/resources/testfiles/file_1.input"),
+				new File("target/concatfile.output"));
+		execute("src/test/resources/test-pom.xml", "concat", "target/concatfile.output",
+				"file1file1file2file3");
+	}
 
-    @Test
-    public void testConcatDeleteTargetFile() throws Exception {
-        FileUtils.copyFile(new File("src/test/resources/testfiles/file_1.input"), new File("target/concatfile.output"));
-        execute("src/test/resources/test-delete-target-file-pom.xml", "concat", "target/concatfile.output",
-                "file1file2file3");
-    }
+	@Test
+	public void testConcatDeleteTargetFile() throws Exception {
+		FileUtils.copyFile(new File("src/test/resources/testfiles/file_1.input"),
+				new File("target/concatfile.output"));
+		execute("src/test/resources/test-delete-target-file-pom.xml", "concat",
+				"target/concatfile.output", "file1file2file3");
+	}
 
-    @Test
-    public void testConcatNewline() throws Exception {
-        final String expectedResult = "file1" + NEWLINE + "file2" + NEWLINE + "file3" + NEWLINE;
-        execute("src/test/resources/test-newline-pom.xml", "concat", "target/concatfile.output", expectedResult);
+	@Test
+	public void testConcatNewline() throws Exception {
+		final String expectedResult = "file1" + NEWLINE + "file2" + NEWLINE + "file3"
+				+ NEWLINE;
+		execute("src/test/resources/test-newline-pom.xml", "concat",
+				"target/concatfile.output", expectedResult);
 
-    }
+	}
 
-    @Test
-    public void testMissingOutputParameter() throws Exception {
-        try {
-            execute("src/test/resources/test-missing-output-pom.xml", "concat", "bla", "nothing");
-        } catch (final Exception e) {
-            assertEquals(MojoExecutionException.class, e.getClass());
-        }
-    }
+	@Test
+	public void testMissingOutputParameter() throws Exception {
+		try {
+			execute("src/test/resources/test-missing-output-pom.xml", "concat", "bla",
+					"nothing");
+		}
+		catch (final Exception e) {
+			assertEquals(MojoExecutionException.class, e.getClass());
+		}
+	}
 
-    @Test
-    public void testMissingConcatParameter() throws Exception {
-        try {
-            execute("src/test/resources/test-missing-concat-pom.xml", "concat", "bla", "nothing");
-        } catch (final Exception e) {
-            ExceptionUtils.printRootCauseStackTrace(e);
-            assertEquals(MojoExecutionException.class, e.getClass());
-        }
-    }
+	@Test
+	public void testMissingConcatParameter() throws Exception {
+		try {
+			execute("src/test/resources/test-missing-concat-pom.xml", "concat", "bla",
+					"nothing");
+		}
+		catch (final Exception e) {
+			ExceptionUtils.printRootCauseStackTrace(e);
+			assertEquals(MojoExecutionException.class, e.getClass());
+		}
+	}
 
-    @Test
-    public void testDirectoryCompetingArgs() throws Exception {
-        try {
-            execute("src/test/resources/test-pom-directory-competing-args.xml", "concat", "bla", "nothing");
-        } catch (final Exception e) {
-            ExceptionUtils.printRootCauseStackTrace(e);
-            assertEquals(MojoExecutionException.class, e.getClass());
-        }
-    }
+	@Test
+	public void testDirectoryCompetingArgs() throws Exception {
+		try {
+			execute("src/test/resources/test-pom-directory-competing-args.xml", "concat",
+					"bla", "nothing");
+		}
+		catch (final Exception e) {
+			ExceptionUtils.printRootCauseStackTrace(e);
+			assertEquals(MojoExecutionException.class, e.getClass());
+		}
+	}
 
-    @Test
-    public void testDirectoryMissingDirectory() throws Exception {
-        try {
-            execute("src/test/resources/test-pom-directory-missing-directory.xml", "concat", "bla", "nothing");
-        } catch (final Exception e) {
-            ExceptionUtils.printRootCauseStackTrace(e);
-            assertEquals(MojoExecutionException.class, e.getClass());
-        }
-    }
+	@Test
+	public void testDirectoryMissingDirectory() throws Exception {
+		try {
+			execute("src/test/resources/test-pom-directory-missing-directory.xml",
+					"concat", "bla", "nothing");
+		}
+		catch (final Exception e) {
+			ExceptionUtils.printRootCauseStackTrace(e);
+			assertEquals(MojoExecutionException.class, e.getClass());
+		}
+	}
 
-    @Test
-    public void testNotADirectory() throws Exception {
-        try {
-            execute("src/test/resources/test-pom-directory-not-a-directory.xml", "concat", "bla", "nothing");
-        } catch (final Exception e) {
-            ExceptionUtils.printRootCauseStackTrace(e);
-            assertEquals(MojoExecutionException.class, e.getClass());
-        }
-    }
+	@Test
+	public void testNotADirectory() throws Exception {
+		try {
+			execute("src/test/resources/test-pom-directory-not-a-directory.xml", "concat",
+					"bla", "nothing");
+		}
+		catch (final Exception e) {
+			ExceptionUtils.printRootCauseStackTrace(e);
+			assertEquals(MojoExecutionException.class, e.getClass());
+		}
+	}
 
-    private void execute(final String pomPath, final String mojo, final String resultFile, final String expectedResult)
-            throws Exception {
-        final File pom = getTestFile(pomPath);
-        assertNotNull(pom);
-        assertTrue(pom.exists());
+	private void execute(final String pomPath, final String mojo, final String resultFile,
+			final String expectedResult) throws Exception {
+		final File pom = getTestFile(pomPath);
+		assertNotNull(pom);
+		assertTrue(pom.exists());
 
-        final ConcatMojo concatMojo = (ConcatMojo) lookupMojo(mojo, pom);
-        assertNotNull(concatMojo);
-        concatMojo.execute();
+		final ConcatMojo concatMojo = (ConcatMojo) lookupMojo(mojo, pom);
+		assertNotNull(concatMojo);
+		concatMojo.execute();
 
-        final File outputFile = getTestFile(resultFile);
-        assertNotNull(outputFile);
-        assertTrue(outputFile.exists());
-        final String output = FileUtils.readFileToString(outputFile, Charset.forName("UTF-8"));
-        assertEquals(expectedResult, output);
-    }
+		final File outputFile = getTestFile(resultFile);
+		assertNotNull(outputFile);
+		assertTrue(outputFile.exists());
+		final String output = FileUtils.readFileToString(outputFile,
+				Charset.forName("UTF-8"));
+		assertEquals(expectedResult, output);
+	}
 
 }
