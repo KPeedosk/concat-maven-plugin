c757a16cc63a71f9af5a04747768febec538e658
Darren Forsythe <forsythed90@gmail.com>
2018-09-20 23:30:54 +0100

    fix mojo class. Formatter moved required java doc layout

--- src/main/java/io/github/flaw101/concat/ConcatMojo.java
+++ src/main/java/io/github/flaw101/concat/ConcatMojo.java
@@ -41,89 +41,90 @@ import org.slf4j.LoggerFactory;
  * Goal which concatenates several files and creates a new file as specified.
  *
  * @author Darren Forsythe
- * @version 1.0.0 @Mojo( name = "concat" )
- * @goal concat @Mojo( defaultPhase = "process-sources" )
+ * @version 1.0.0
+ * @Mojo( name = "concat" )
+ * @goal concat
+ * @Mojo( defaultPhase = "process-sources" )
  * @phase process-sources
  * @since 1.0.0
  */
 public class ConcatMojo extends AbstractMojo {
 
-	private static final Logger logger = LoggerFactory.getLogger(ConcatMojo.class);
+    private static final Logger logger = LoggerFactory.getLogger(ConcatMojo.class);
 
-	/**
-	 * Type of concatenation to perform
-	 *
-	 * @parameter
-	 * @required
-	 */
-	private ConcatenationType concatenationType;
+    /**
+     * Type of concatenation to perform
+     *
+     * @parameter
+     * @required
+     */
+    private ConcatenationType concatenationType;
 
-	/**
-	 * The resulting file
-	 *
-	 * @parameter
-	 * @required
-	 */
-	private File outputFile;
+    /**
+     * The resulting file
+     *
+     * @parameter
+     * @required
+     */
+    private File outputFile;
 
-	/**
-	 * Files to concatenate if using {@link ConcatenationType#FILE_LIST}.
-	 *
-	 * @parameter
-	 */
-	private List<File> concatFiles;
+    /**
+     * Files to concatenate if using {@link ConcatenationType#FILE_LIST}.
+     *
+     * @parameter
+     */
+    private List<File> concatFiles;
 
-	/**
-	 * If using the {@link ConcatenationType#DIRECTORY} provide a directory of which all
-	 * files contained within it will be concatenated in natural ordering.
-	 *
-	 * @parameter
-	 */
-	private String directory;
+    /**
+     * If using the {@link ConcatenationType#DIRECTORY} provide a directory of which all
+     * files contained within it will be concatenated in natural ordering.
+     *
+     * @parameter
+     */
+    private String directory;
 
-	/**
-	 * Append newline after each concatenation
-	 *
-	 * @parameter default-value="false"
-	 */
-	private boolean appendNewline;
+    /**
+     * Append newline after each concatenation
+     *
+     * @parameter default-value="false"
+     */
+    private boolean appendNewline;
 
-	/**
-	 * Deletes the target file before concatenation
-	 *
-	 * @parameter default-value="false"
-	 */
-	private boolean deleteTargetFile;
+    /**
+     * Deletes the target file before concatenation
+     *
+     * @parameter default-value="false"
+     */
+    private boolean deleteTargetFile;
 
-	/**
-	 * If using {@link ConcatenationType#DIRECTORY} you can specify the file you wish to
-	 * start, the rest will follow in natural order.
-	 *
-	 * @parameter
-	 */
-	private File startingFile;
+    /**
+     * If using {@link ConcatenationType#DIRECTORY} you can specify the file you wish to
+     * start, the rest will follow in natural order.
+     *
+     * @parameter
+     */
+    private File startingFile;
 
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see org.apache.maven.plugin.AbstractMojo#execute()
-	 */
-	@Override
-	public void execute() throws MojoExecutionException {
-		final ConcatParams params = new ConcatParams(directory, concatFiles, outputFile,
-				deleteTargetFile, appendNewline, concatenationType, startingFile);
-		logger.info("Starting Concatenation");
-		logger.debug("Concatenating with params - {}", params);
+    /*
+     * (non-Javadoc)
+     *
+     * @see org.apache.maven.plugin.AbstractMojo#execute()
+     */
+    @Override
+    public void execute() throws MojoExecutionException {
+        final ConcatParams params = new ConcatParams(directory, concatFiles, outputFile,
+                deleteTargetFile, appendNewline, concatenationType, startingFile);
+        logger.info("Starting Concatenation");
+        logger.debug("Concatenating with params - {}", params);
 
-		final Injector injector = Guice.createInjector(new ConcatModule());
-		final ConcatService concatService = injector.getInstance(ConcatService.class);
-		try {
-			concatService.concat(params);
-		}
-		catch (final Exception e) {
-			logger.error("Failed to execute concat plugin!", e);
-			throw new MojoExecutionException("Failed to concat", e);
-		}
-	}
+        final Injector injector = Guice.createInjector(new ConcatModule());
+        final ConcatService concatService = injector.getInstance(ConcatService.class);
+        try {
+            concatService.concat(params);
+        } catch (final Exception e) {
+            logger.error("Failed to execute concat plugin!", e);
+            throw new MojoExecutionException("Failed to concat", e);
+        }
+    }
 
 }
