06a83a7235fb33add0ca53dc491842c271e3a255
Darren Forsythe <forsythed90@gmail.com>
2018-11-14 00:46:59 +0000

    initial wor for semver concat. This assumes directory as file list is explicit i.e. you can just do it with config in POM

---
+++ src/main/java/io/github/flaw101/concat/filewriter/setup/SemanticVersioningCompator.java
@@ -0,0 +1,146 @@
+package io.github.flaw101.concat.filewriter.setup;
+
+import java.io.File;
+import java.util.Comparator;
+
+/**
+ * Happily Copied and Pasted from <a href="https://stackoverflow.com/a/10034633/7554328">StackoverFlow</a> with a tiny
+ * reimplementation.
+ *
+ * @author Darren Forsythe
+ * @since 1.5.0
+ */
+public class SemanticVersioningCompator implements Comparator<File> {
+
+    @Override
+    public int compare(File file, File t1) {
+
+        VersionTokenizer tokenizer1 = new VersionTokenizer(file.getName());
+        VersionTokenizer tokenizer2 = new VersionTokenizer(t1.getName());
+
+        int number1 = 0, number2 = 0;
+        String suffix1 = "", suffix2 = "";
+
+        while (tokenizer1.MoveNext()) {
+            if (!tokenizer2.MoveNext()) {
+                do {
+                    number1 = tokenizer1.getNumber();
+                    suffix1 = tokenizer1.getSuffix();
+                    if (number1 != 0 || suffix1.length() != 0) {
+                        // Version one is longer than number two, and non-zero
+                        return 1;
+                    }
+                }
+                while (tokenizer1.MoveNext());
+
+                // Version one is longer than version two, but zero
+                return 0;
+            }
+
+            number1 = tokenizer1.getNumber();
+            suffix1 = tokenizer1.getSuffix();
+            number2 = tokenizer2.getNumber();
+            suffix2 = tokenizer2.getSuffix();
+
+            if (number1 < number2) {
+                // Number one is less than number two
+                return -1;
+            }
+            if (number1 > number2) {
+                // Number one is greater than number two
+                return 1;
+            }
+
+            boolean empty1 = suffix1.length() == 0;
+            boolean empty2 = suffix2.length() == 0;
+
+            if (empty1 && empty2) continue; // No suffixes
+            if (empty1) return 1; // First suffix is empty (1.2 > 1.2b)
+            if (empty2) return -1; // Second suffix is empty (1.2a < 1.2)
+
+            // Lexical comparison of suffixes
+            int result = suffix1.compareTo(suffix2);
+            if (result != 0) return result;
+
+        }
+        if (tokenizer2.MoveNext()) {
+            do {
+                number2 = tokenizer2.getNumber();
+                suffix2 = tokenizer2.getSuffix();
+                if (number2 != 0 || suffix2.length() != 0) {
+                    // Version one is longer than version two, and non-zero
+                    return -1;
+                }
+            }
+            while (tokenizer2.MoveNext());
+
+            // Version two is longer than version one, but zero
+            return 0;
+        }
+        return 0;
+    }
+}
+
+class VersionTokenizer {
+    private final String _versionString;
+    private final int _length;
+
+    private int _position;
+    private int _number;
+    private String _suffix;
+    private boolean _hasValue;
+
+    public int getNumber() {
+        return _number;
+    }
+
+    String getSuffix() {
+        return _suffix;
+    }
+
+    public boolean hasValue() {
+        return _hasValue;
+    }
+
+    VersionTokenizer(String versionString) {
+        if (versionString == null)
+            throw new IllegalArgumentException("versionString is null");
+
+        _versionString = versionString;
+        _length = versionString.length();
+    }
+
+    boolean MoveNext() {
+        _number = 0;
+        _suffix = "";
+        _hasValue = false;
+
+        // No more characters
+        if (_position >= _length)
+            return false;
+
+        _hasValue = true;
+
+        while (_position < _length) {
+            char c = _versionString.charAt(_position);
+            if (c < '0' || c > '9') break;
+            _number = _number * 10 + (c - '0');
+            _position++;
+        }
+
+        int suffixStart = _position;
+
+        while (_position < _length) {
+            char c = _versionString.charAt(_position);
+            if (c == '.') break;
+            _position++;
+        }
+
+        _suffix = _versionString.substring(suffixStart, _position);
+
+        if (_position < _length) _position++;
+
+        return true;
+    }
+}
+
---
+++ src/main/java/io/github/flaw101/concat/filewriter/setup/StartingFileHandler.java
@@ -0,0 +1,31 @@
+package io.github.flaw101.concat.filewriter.setup;
+
+import io.github.flaw101.concat.ConcatParams;
+import org.slf4j.Logger;
+
+import java.io.File;
+import java.util.Iterator;
+import java.util.List;
+
+public class StartingFileHandler {
+
+    //TODO Refactor
+    public static void setStartingFileToStartOfFiles(ConcatParams params, List<File> listFiles, Logger logger) {
+        if (params.getStartingFile() != null) {
+            Iterator<File> fileIterator = listFiles.iterator();
+            File file;
+            logger.debug("Starting File not null. Placing - {} at start of list",
+                    params.getStartingFile());
+            while (fileIterator.hasNext()) {
+                file = fileIterator.next();
+                if (file.getAbsolutePath()
+                        .equals(params.getStartingFile().getAbsolutePath())) {
+                    logger.debug("Removing file to file place at start of list - {}",
+                            file);
+                    fileIterator.remove();
+                }
+            }
+            listFiles.add(0, params.getStartingFile());
+        }
+    }
+}
